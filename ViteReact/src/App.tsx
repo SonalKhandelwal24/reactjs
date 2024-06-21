2bde519fd4194b0fc45a1f9d314d702975ef3aa6
Sonal Khandelwal <ksonalkhandelwal124@gmail.com>
2024-06-21 10:04:55 +0530

    Initial commit

---
+++ ViteReact/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+    <h2>Hello World!!</h2>
+    <Hello />
+    </>
+  )
+}
+
+export default App;
+import Hello from "./Hello";
---
+++ ViteReact/src/Hello.tsx
@@ -0,0 +1,7 @@
+function Hello () {
+    return(
+        <h2>Hii!! Sonal Khandelwal</h2>
+    )
+}
+
+export default Hello;
