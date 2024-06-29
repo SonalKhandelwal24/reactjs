6d23125569a6a6450268e94273e5affd5c963191
Sonal Khandelwal <ksonalkhandelwal124@gmail.com>
2024-06-29 11:38:40 +0530

    ReduxToolkit

--- reduxToolkitTodo/src/components/Todos.tsx
+++ reduxToolkitTodo/src/components/Todos.tsx
@@ -1,54 +1,107 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { deleteTodo, removeTodo } from '../features/todo/todoSlice'
+import { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteTodo, removeTodo, updateTodo } from '../features/todo/todoSlice.ts';
 
-function Todos() {
+function AddTodo() {
+  const [editInput, setEditInput] = useState('');
+  const [editId, setEditId] = useState(null);
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const listRef = useRef(null);
 
-  const todos = useSelector(state => state.todos)
-  const dispatch = useDispatch()
-
+  
+  const updateTodoHandler = (e: any, id: string) => {
+    e.preventDefault();
+    if (editInput.trim()) {
+      dispatch(updateTodo({ id, text: editInput }));
+      setEditId(null);
+      setEditInput('');
+    }
+  };
 
   return (
     <>
-      <div className='text-2xl font-bold p-4'>Todos</div>
+      {/* Show todo items */}
+      <div className="text-2xl font-bold p-4">Todos</div>
       <ul className="list-none">
         {todos.map((todo: any) => (
           <li
             className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
             key={todo.id}
           >
-            <div className='text-white'>{todo.text}</div>
-            
-            <button
-              onClick={() => dispatch(removeTodo(todo.id))}
-              className="text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-blue-600  active:bg-white active:text-black rounded text-md"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            {/* Update todo items */}
+            {editId === todo.id ? (
+              <form onSubmit={(e) => updateTodoHandler(e, todo.id)} className="flex flex-1">
+                <input
+                  type="text"
+                  className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out flex-1 mr-2"
+                  value={editInput}
+                  onChange={(e) => setEditInput(e.target.value)}
                 />
-              </svg>
-            </button>
+                <button
+                  type="submit"
+                  className="text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-green-600 active:bg-white active:text-black rounded text-md"
+                >
+                  Save
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setEditId(null)}
+                  className="text-white bg-gray-500 border-0 py-1 px-4 focus:outline-none hover:bg-gray-600 active:bg-white active:text-black rounded text-md"
+                >
+                  Cancel
+                </button>
+              </form>
+            ) : (
+              <>
+                <div className="text-white flex-1" ref={listRef}>{todo.text}</div>
+                <div className="flex space-x-2">
+                  <button
+                    onClick={() => {
+                      setEditId(todo.id);
+                      setEditInput(todo.text);
+                    }}
+                    className="text-white bg-green-500 border-0 py-1 px-4 focus:outline-none hover:bg-green-600 active:bg-white active:text-black rounded text-md"
+                  >
+                    Update
+                  </button>
+
+                  {/* Delete todo item */}
+                  <button
+                    onClick={() => dispatch(deleteTodo(todo.id))}
+                    className="text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-blue-600 active:bg-white active:text-black rounded text-md"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </>
+            )}
           </li>
         ))}
       </ul>
 
+      {/* Remove all todo items */}
       <button
-        onClick={() => dispatch(deleteTodo())}
+        onClick={() => dispatch(removeTodo())}
         className="text-white bg-red-800 border-0 py-2 px-4 my-4 focus:outline-none hover:bg-red-700 active:bg-black rounded text-lg font-semibold"
       >
         Remove All
       </button>
     </>
-  )
+  );
 }
 
-export default Todos
+export default AddTodo;
--- reduxToolkitTodo/src/features/todo/todoSlice.ts
+++ reduxToolkitTodo/src/features/todo/todoSlice.ts
@@ -22,22 +22,15 @@ export const todoSlice = createSlice({
             state.todos.push(newTodo);
         },
 
-        removeTodo: (state, action) => {
+        deleteTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload);
         },
         
-        // updateTodo: (state, action) => {
-        //     state.todos = state.todos.map((todo) => todo.id === action.payload ? { text: action.payload.text } : todo)
-        // },
         updateTodo: (state, action) => {
-            const { id, text } = action.payload;
-            const todo = state.todos.find((todo) => todo.id === id);
-            if (todo) {
-                todo.text = text;
-            }
+           state.todos.map((todo) => todo.id === action.payload.id ? {...todo,  text: action.payload.text } : todo)
         },
-
-        deleteTodo: (state) => {
+        
+        removeTodo: (state) => {
             state.todos = []
         }
     },
