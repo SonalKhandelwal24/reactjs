3522dea9b1771aa63f075836a4df3cdec4b54711
Sonal Khandelwal <ksonalkhandelwal124@gmail.com>
2024-07-04 14:41:38 +0530

    projects

--- reactRouter/src/components/Index.tsx
+++
@@ -1,8 +0,0 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import About from "./About/About";
-import ContactUs from "./ContactUs/ContactUs";
-import { Layout } from "./Layout";
-import Home from "./Home/Home";
-
-export default {Header, Footer, About, ContactUs, Home, Layout};
