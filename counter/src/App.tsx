708de3e5fb971b3cb09a1dbe941761f67be38c4e
Sonal Khandelwal <ksonalkhandelwal124@gmail.com>
2024-07-04 14:39:39 +0530

    projects

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState, } from 'react'
 import './App.css'
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+ 
 function App() {
 
   let [counter, setCounter] = useState(1)
@@ -9,11 +11,11 @@ function App() {
   // let counter = 1;
 
 
+  const notify = () => toast("value don't exceeds above 10!!");
   const addValue = () => {
     if (counter >= 10) {
-      setMessage("value don't exceeds above 10");
-      // alert("Limit exceeds!!");
-
+      // setMessage("value don't exceeds above 10");
+      {notify};
     } else {
       setCounter(counter => counter + 1);
     }
@@ -21,8 +23,7 @@ function App() {
 
   const removeValue = () => {
     if (counter <= 0) {
-      setMessage(" value less than zero");
-      // alert("Value can't be negative!!");
+      // setMessage(" value less than zero");
     } else {
       setCounter(counter - 1);
       console.log(counter - 1);
@@ -35,9 +36,9 @@ function App() {
       <h2>Counter Value : {counter}</h2>
       <button onClick={addValue} >Add Value </button>
       <br />
-      <button onClick={removeValue} >Remove Value </button>
+      <button onClick={removeValue}>Remove Value </button>
       <br />
-      <span>{message}</span>
+      <ToastContainer />
     </>
   )
 }
